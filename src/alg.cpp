f6c4a8c27b036b5c7d7ba83622e16ad6422c65ac
LogutovIlja <99278246+LogutovIlja@users.noreply.github.com>
2022-04-30 23:53:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,7 +3,7 @@
 int cbinsearch(int *arr, int size, int value) {
   int n = 0;
     int k = size - 1;
-    for (int i = 0; i <= k - 1; i++) {
+    for (int i = 0; i <= k; i++) {
         if (value == arr[i]) n = n + 1;
     }
     return n;
