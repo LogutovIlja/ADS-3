86318682b0dda83a0bf60640a68bdd7401035de0
LogutovIlja <99278246+LogutovIlja@users.noreply.github.com>
2022-04-30 23:48:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,10 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int n = 0;
+    int k = size - 1;
+    for (int i = 0; i <= k - 1; i++) {
+        if (value == arr[i]) n = n + 1;
+    }
+    return n;
 }
